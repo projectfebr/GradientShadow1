b644f580e45dcad022a61e64cf03f4abe90fa692
Aleksandr Bolotov <bolotov@siberian.pro>
2023-07-03 22:17:01 +0300

    answer

--- GradientShadow1/ViewController.swift
+++ GradientShadow1/ViewController.swift
@@ -8,45 +8,72 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private lazy var gradientShadowView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
+    private lazy var cyanView = View()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.addSubview(gradientShadowView)
-        
+        cyanView.backgroundColor = .systemCyan
+        cyanView.layer.cornerRadius = 14
+        cyanView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(cyanView)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
-            gradientShadowView.widthAnchor.constraint(equalToConstant: 100),
-            gradientShadowView.heightAnchor.constraint(equalToConstant: 100),
-            gradientShadowView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
-            gradientShadowView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            cyanView.widthAnchor.constraint(equalToConstant: 100),
+            cyanView.heightAnchor.constraint(equalToConstant: 100),
+            cyanView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            cyanView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        setupGradientShadow()
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        cyanView.frame = .init(x: 100, y: cyanView.frame.origin.y, width: 120, height: 120)
+//        cyanView.center.y = view.frame.height/2
+//    }
+    
+}
+
+class View: UIView {
+    let gradientLayer = CAGradientLayer()
+    
+    init() {
+        super.init(frame: .zero)
+        
+        backgroundColor = .systemCyan
+        // TODO: что за свойство понять
+        layer.cornerCurve = .continuous
+        
+//        layer.masksToBounds = true // wrong
+        
+        /// тень
+        layer.shadowOpacity = 0.6
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = .zero
+        layer.shadowRadius = 12
+        
+        gradientLayer.startPoint = .init(x: 0, y: 0)
+        gradientLayer.endPoint = .init(x: 1, y: 0.5)
+        gradientLayer.colors = [
+            UIColor.systemRed.withAlphaComponent(0.1).cgColor,
+            UIColor.systemRed.cgColor
+        ]
+        layer.addSublayer(gradientLayer)
+
     }
     
-    private func setupGradientShadow() {
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [
-            UIColor(red: 65/255, green: 72/255, blue: 86/255, alpha: 1).cgColor,
-            UIColor(red: 252/255, green: 240/255, blue: 130/255, alpha: 1).cgColor
-        ]
-        gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
-        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.0)
-        gradientLayer.frame = gradientShadowView.bounds
-        gradientLayer.cornerRadius = 20
-        gradientShadowView.layer.shadowOffset = CGSize(width: 0, height: 2)
-        gradientShadowView.layer.shadowOpacity = 0.8
-        gradientShadowView.layer.shadowRadius = 20.0
-        gradientShadowView.layer.shadowColor = UIColor(red: 30/255, green: 36/255, blue: 50/255, alpha: 1).cgColor
-        gradientShadowView.layer.masksToBounds = false
-        gradientShadowView.layer.insertSublayer(gradientLayer, at: 0)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // TODO: не очень понятно, подумать, удалить ттуду 
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        if layer == self.layer {
+            gradientLayer.frame = bounds
+            gradientLayer.cornerRadius = layer.cornerRadius
+        }
     }
 }
